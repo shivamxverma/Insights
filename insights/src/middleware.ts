89a698af9504d762fcbe7f9994db0c23e779b7cb
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-03-06 22:56:59 +0530

    Added middleware

---
+++ insights/src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import * as cookie from "cookie";
+// import { getAuthSession } from "./lib/auth";
+
+export function middleware(request: NextRequest) {
+  const cookies = cookie.parse(request.headers.get("cookie") || "");
+  // const session = getAuthSession
+
+  const sessionToken = cookies["next-auth.session-token"] || cookies["__Secure-next-auth.session-token"];
+
+  const protectedRoutes = ["/notes", "/yt-video"];
+
+  if (protectedRoutes.some((route) => request.nextUrl.pathname.startsWith(route))) {
+    if (!sessionToken) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/notes/:path*", "/yt-video/:path*"],
+};
